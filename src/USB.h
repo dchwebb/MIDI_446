1a86812b77a59a518b13bf194c668c86a768f35e
Dominic Webb <dwebb@reapit.com>
2019-10-18 18:22:20 +0100

    Updates to imterrupt action list; added midi configuration descriptor

--- src/USB.h
+++ src/USB.h
@@ -10,98 +10,101 @@ extern uint8_t usbEventNo, eventOcc;
 #define USBx_DEVICE     ((USB_OTG_DeviceTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_DEVICE_BASE))
 #define USBx_INEP(i)    ((USB_OTG_INEndpointTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_IN_ENDPOINT_BASE + ((i) * USB_OTG_EP_REG_SIZE)))
 #define USBx_OUTEP(i)   ((USB_OTG_OUTEndpointTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_OUT_ENDPOINT_BASE + ((i) * USB_OTG_EP_REG_SIZE)))
-//#define USBx_DFIFO(i)   *(__IO uint32_t *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_FIFO_BASE + ((i) * USB_OTG_FIFO_SIZE))
 #define USBx_DFIFO(i)   *(uint32_t*)(USB_OTG_FS_PERIPH_BASE + USB_OTG_FIFO_BASE + ((i) * USB_OTG_FIFO_SIZE))
 
 
 // USB Transfer status definitions
-#define STS_GOUT_NAK                           1U
-#define STS_DATA_UPDT                          2U
-#define STS_XFER_COMP                          3U
-#define STS_SETUP_COMP                         4U
-#define STS_SETUP_UPDT                         6U
+#define STS_GOUT_NAK					1U
+#define STS_DATA_UPDT					2U
+#define STS_XFER_COMP					3U
+#define STS_SETUP_COMP					4U
+#define STS_SETUP_UPDT					6U
 
 // USB Request Recipient types
-#define USB_REQ_RECIPIENT_DEVICE              0x00U
-#define USB_REQ_RECIPIENT_INTERFACE           0x01U
-#define USB_REQ_RECIPIENT_ENDPOINT            0x02U
-#define USB_REQ_RECIPIENT_MASK                0x03U
+#define USB_REQ_RECIPIENT_DEVICE		0x00U
+#define USB_REQ_RECIPIENT_INTERFACE		0x01U
+#define USB_REQ_RECIPIENT_ENDPOINT		0x02U
+#define USB_REQ_RECIPIENT_MASK			0x03U
 
-#define EP_ADDR_MSK                            0xFU
+#define EP_ADDR_MSK						0xFU
 
 // USB Request types
-#define USB_REQ_TYPE_STANDARD                          0x00U
-#define USB_REQ_TYPE_CLASS                             0x20U
-#define USB_REQ_TYPE_VENDOR                            0x40U
-#define USB_REQ_TYPE_MASK                              0x60U
+#define USB_REQ_TYPE_STANDARD			0x00U
+#define USB_REQ_TYPE_CLASS				0x20U
+#define USB_REQ_TYPE_VENDOR				0x40U
+#define USB_REQ_TYPE_MASK				0x60U
 
-#define USB_REQ_GET_STATUS                             0x00U
-#define USB_REQ_CLEAR_FEATURE                          0x01U
-#define USB_REQ_SET_FEATURE                            0x03U
-#define USB_REQ_SET_ADDRESS                            0x05U
-#define USB_REQ_GET_DESCRIPTOR                         0x06U
-#define USB_REQ_SET_DESCRIPTOR                         0x07U
-#define USB_REQ_GET_CONFIGURATION                      0x08U
-#define USB_REQ_SET_CONFIGURATION                      0x09U
-#define USB_REQ_GET_INTERFACE                          0x0AU
-#define USB_REQ_SET_INTERFACE                          0x0BU
-#define USB_REQ_SYNCH_FRAME                            0x0CU
+#define USB_REQ_GET_STATUS				0x00U
+#define USB_REQ_CLEAR_FEATURE			0x01U
+#define USB_REQ_SET_FEATURE				0x03U
+#define USB_REQ_SET_ADDRESS				0x05U
+#define USB_REQ_GET_DESCRIPTOR			0x06U
+#define USB_REQ_SET_DESCRIPTOR			0x07U
+#define USB_REQ_GET_CONFIGURATION		0x08U
+#define USB_REQ_SET_CONFIGURATION		0x09U
+#define USB_REQ_GET_INTERFACE			0x0AU
+#define USB_REQ_SET_INTERFACE			0x0BU
+#define USB_REQ_SYNCH_FRAME				0x0CU
 
-#define USBD_IDX_LANGID_STR                            0x00U
-#define USBD_IDX_MFC_STR                               0x01U
-#define USBD_IDX_PRODUCT_STR                           0x02U
-#define USBD_IDX_SERIAL_STR                            0x03U
-#define USBD_IDX_CONFIG_STR                            0x04U
-#define USBD_IDX_INTERFACE_STR                         0x05U
+#define USBD_IDX_LANGID_STR				0x00U
+#define USBD_IDX_MFC_STR				0x01U
+#define USBD_IDX_PRODUCT_STR			0x02U
+#define USBD_IDX_SERIAL_STR				0x03U
+#define USBD_IDX_CONFIG_STR				0x04U
+#define USBD_IDX_INTERFACE_STR			0x05U
 
-#define USB_DESC_TYPE_DEVICE                           0x01U
-#define USB_DESC_TYPE_CONFIGURATION                    0x02U
-#define USB_DESC_TYPE_STRING                           0x03U
-#define USB_DESC_TYPE_INTERFACE                        0x04U
-#define USB_DESC_TYPE_ENDPOINT                         0x05U
-#define USB_DESC_TYPE_DEVICE_QUALIFIER                 0x06U
+#define USB_DESC_TYPE_DEVICE			0x01U
+#define USB_DESC_TYPE_CONFIGURATION		0x02U
+#define USB_DESC_TYPE_STRING			0x03U
+#define USB_DESC_TYPE_INTERFACE			0x04U
+#define USB_DESC_TYPE_ENDPOINT			0x05U
+#define USB_DESC_TYPE_DEVICE_QUALIFIER	0x06U
 #define USB_DESC_TYPE_OTHER_SPEED_CONFIGURATION        0x07U
-#define USB_DESC_TYPE_BOS                              0x0FU
+#define USB_DESC_TYPE_BOS				0x0FU
 
-#define USB_LEN_DEV_QUALIFIER_DESC                     0x0AU
-#define USB_LEN_DEV_DESC                               0x12U
-#define USB_LEN_CFG_DESC                               0x09U
-#define USB_LEN_IF_DESC                                0x09U
-#define USB_LEN_EP_DESC                                0x07U
-#define USB_LEN_OTG_DESC                               0x03U
-#define USB_LEN_LANGID_STR_DESC                        0x04U
-#define USB_LEN_OTHER_SPEED_DESC_SIZ                   0x09U
-#define USBD_MAX_STR_DESC_SIZ     						512U
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE				0x4A
+#define USB_LEN_DEV_QUALIFIER_DESC		0x0AU
+#define USB_LEN_DEV_DESC				0x12U
+#define USB_LEN_CFG_DESC				0x09U
+#define USB_LEN_IF_DESC					0x09U
+#define USB_LEN_EP_DESC					0x07U
+#define USB_LEN_OTG_DESC				0x03U
+#define USB_LEN_LANGID_STR_DESC			0x04U
+#define USB_LEN_OTHER_SPEED_DESC_SIZ	0x09U
+#define USBD_MAX_STR_DESC_SIZ     		512U
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE	0x4A
 
 // EP0 State
-#define USBD_EP0_IDLE                                   0x00U
-#define USBD_EP0_SETUP                                  0x01U
-#define USBD_EP0_DATA_IN                                0x02U
-#define USBD_EP0_DATA_OUT                               0x03U
-#define USBD_EP0_STATUS_IN                              0x04U
-#define USBD_EP0_STATUS_OUT                             0x05U
-#define USBD_EP0_STALL                                  0x06U
+#define USBD_EP0_IDLE					0x00U
+#define USBD_EP0_SETUP					0x01U
+#define USBD_EP0_DATA_IN				0x02U
+#define USBD_EP0_DATA_OUT				0x03U
+#define USBD_EP0_STATUS_IN				0x04U
+#define USBD_EP0_STATUS_OUT				0x05U
+#define USBD_EP0_STALL					0x06U
 
-#define USBD_EP_TYPE_CTRL                               0x00U
-#define USBD_EP_TYPE_ISOC                               0x01U
-#define USBD_EP_TYPE_BULK                               0x02U
-#define USBD_EP_TYPE_INTR                               0x03U
+#define USBD_EP_TYPE_CTRL				0x00U
+#define USBD_EP_TYPE_ISOC				0x01U
+#define USBD_EP_TYPE_BULK				0x02U
+#define USBD_EP_TYPE_INTR				0x03U
 
 //  Device Status
-#define USBD_STATE_DEFAULT                              0x01U
-#define USBD_STATE_ADDRESSED                            0x02U
-#define USBD_STATE_CONFIGURED                           0x03U
-#define USBD_STATE_SUSPENDED                            0x04U
+#define USBD_STATE_DEFAULT				0x01U
+#define USBD_STATE_ADDRESSED			0x02U
+#define USBD_STATE_CONFIGURED			0x03U
+#define USBD_STATE_SUSPENDED			0x04U
 
 
-#define USBD_VID     1155
-#define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "ButtFluffkins"
-#define USBD_PID_FS     22352
-#define USBD_PRODUCT_STRING_FS     "STM32 Custom Human interface"
-#define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
-#define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
+#define USBD_VID						1155
+#define USBD_LANGID_STRING				1033
+#define USBD_MANUFACTURER_STRING		"Mountjoy Modular"
+#define USBD_PID_FS						22352
+#define USBD_PRODUCT_STRING_FS			"Mountjoy MIDI"
+
+#define CLASS_SPECIFIC_DESC_SIZE		(7+6+9+9+5+9+5)
+#define MIDI_CONFIG_DESC_SIZE 			(9+9+9+9+CLASS_SPECIFIC_DESC_SIZE)
+#define USB_DEVICE_CLASS_AUDIO			0x01
+#define USB_DEVICE_SUBCLASS_MIDISTREAMING      0x03
+
 
 #define SWAPBYTE(addr)        (((uint16_t)(*((uint8_t *)(addr)))) + \
 		(((uint16_t)(*(((uint8_t *)(addr)) + 1U))) << 8U))
@@ -164,7 +167,7 @@ public:
 			0x00,					// bDeviceClass
 			0x00,					// bDeviceSubClass
 			0x00,					// bDeviceProtocol
-			64,  				 	// bMaxPacketSize
+			64,  					// bMaxPacketSize
 			LOBYTE(USBD_VID),		// idVendor
 			HIBYTE(USBD_VID),		// idVendor
 			LOBYTE(USBD_PID_FS),	// idProduct
@@ -177,82 +180,134 @@ public:
 			1						// bNumConfigurations
 	};
 
-	// USB CUSTOM_HID device FS Configuration Descriptor  - in usbd_customhid.c
-	uint8_t USBD_CUSTOM_HID_CfgFSDesc[0x29] = {
-			0x09, 					// bLength: Configuration Descriptor size
-			USB_DESC_TYPE_CONFIGURATION, // bDescriptorType: Configuration
-			0x29, 					// USB_CUSTOM_HID_CONFIG_DESC_SIZ, wTotalLength: Bytes returned
-			0x00,
-			0x01,         			// bNumInterfaces: 1 interface
-			0x01,         /*bConfigurationValue: Configuration value*/
-			0x00,         /*iConfiguration: Index of string descriptor describing
-	  the configuration*/
-			0xC0,         /*bmAttributes: bus powered */
-			0x32,         /*MaxPower 100 mA: this current is used for detecting Vbus*/
 
-			/************** Descriptor of CUSTOM HID interface ****************/
-			/* 09 */
-			0x09,         /*bLength: Interface Descriptor size*/
-			USB_DESC_TYPE_INTERFACE,/*bDescriptorType: Interface descriptor type*/
-			0x00,         /*bInterfaceNumber: Number of Interface*/
-			0x00,         /*bAlternateSetting: Alternate setting*/
-			0x02,         /*bNumEndpoints*/
-			0x03,         /*bInterfaceClass: CUSTOM_HID*/
-			0x00,         /*bInterfaceSubClass : 1=BOOT, 0=no boot*/
-			0x00,         /*nInterfaceProtocol : 0=none, 1=keyboard, 2=mouse*/
-			0,            /*iInterface: Index of string descriptor*/
-			/******************** Descriptor of CUSTOM_HID *************************/
-			/* 18 */
-			0x09,         /*bLength: CUSTOM_HID Descriptor size*/
-			0x21, 		/*bDescriptorType: CUSTOM_HID*/
-			0x11,         /*bCUSTOM_HIDUSTOM_HID: CUSTOM_HID Class Spec release number*/
-			0x01,
-			0x00,         /*bCountryCode: Hardware target country*/
-			0x01,         /*bNumDescriptors: Number of CUSTOM_HID class descriptors to follow*/
-			0x22,         /*bDescriptorType*/
-			0x4A, 		// USBD_CUSTOM_HID_REPORT_DESC_SIZE,/*wItemLength: Total length of Report descriptor*/
-			0x00,
-			/******************** Descriptor of Custom HID endpoints ********************/
-			/* 27 */
-			0x07,          /*bLength: Endpoint Descriptor size*/
-			USB_DESC_TYPE_ENDPOINT, /*bDescriptorType:*/
+	// USB AUDIO device Configuration Descriptor
+	uint8_t usbd_audio_CfgDesc[MIDI_CONFIG_DESC_SIZE] =	{
 
-			0x81, 		// CUSTOM_HID_EPIN_ADDR,     /*bEndpointAddress: Endpoint Address (IN)*/
-			0x03,          /*bmAttributes: Interrupt endpoint*/
-			0x04, 		// CUSTOM_HID_EPIN_SIZE, wMaxPacketSize: 2 Byte max
-			0x00,
-			0x05, 		// CUSTOM_HID_FS_BINTERVAL,          bInterval: Polling Interval
-			/* 34 */
+			0x09,							// bLength
+			USB_DESC_TYPE_CONFIGURATION,	// bDescriptorType
+			LOBYTE(MIDI_CONFIG_DESC_SIZE),	// wTotalLength
+			HIBYTE(MIDI_CONFIG_DESC_SIZE),
+			0x02,							// bNumInterfaces
+			0x01,							// bConfigurationValue
+			0x00,							// iConfiguration
+			0xC0,							// bmAttributes  = b6 + b7 : self powered
+			0x32,							// bMaxPower = 100 mA
 
-			0x07,	         /* bLength: Endpoint Descriptor size */
-			USB_DESC_TYPE_ENDPOINT,	/* bDescriptorType: */
-			0x01, 		// CUSTOM_HID_EPOUT_ADDR,  bEndpointAddress: Endpoint Address (OUT)
-			0x03,	/* bmAttributes: Interrupt endpoint */
-			0x04, 		// CUSTOM_HID_EPOUT_SIZE,	/* wMaxPacketSize: 2 Bytes max  */
-			0x00,
-			0x05			//CUSTOM_HID_FS_BINTERVAL,	/* bInterval: Polling Interval */
-			/* 41 */
+			// B.3.1 Standard Audio Control standard Interface Descriptor
+			0x09,							// sizeof(usbDescrInterface): length of descriptor in bytes
+			0x04,							// interface descriptor type
+			0x00,							// index of this interface
+			0x00,							// alternate setting for this interface
+			0x00,							// endpoints excl 0: number of endpoint descriptors to follow
+			0x01,							// AUDIO
+			0x01,							// AUDIO_Control
+			0x00,							// bInterfaceProtocol
+			0x00,							// string index for interface
+
+			// B.3.2 Class-specific AC Interface Descriptor
+			0x09,							// sizeof(usbDescrCDC_HeaderFn): length of descriptor in bytes
+			0x24,							// descriptor type
+			0x01,							// header functional descriptor
+			0x00, 0x01,						// bcdADC
+			0x09, 0x00,						// wTotalLength
+			0x01,							// bInCollection
+			0x01,							// baInterfaceNr[1]
+
+			// B.4 MIDIStreaming Interface Descriptors
+
+			// B.4.1 Standard MS Interface Descriptor
+			0x09,							// bLength
+			0x04,							// bDescriptorType
+			0x01,							// bInterfaceNumber
+			0x00,							// bAlternateSetting
+			0x02,							// bNumEndpoints
+			USB_DEVICE_CLASS_AUDIO,			// bInterfaceClass
+			USB_DEVICE_SUBCLASS_MIDISTREAMING,	// bInterfaceSubClass : MIDIStreaming
+			0x00,							// InterfaceProtocol: NOT USED
+			0x00,							// iInterface : NOT USED
+
+			// B.4.2 Class-specific MS Interface Descriptor
+			0x07,							// length of descriptor in bytes
+			0x24,							// descriptor type
+			0x01,							// header functional descriptor
+			0x0, 0x01,						// bcdADC
+			CLASS_SPECIFIC_DESC_SIZE, 0,	// wTotalLength : Why 0x41 ?? XH : don't know...
+
+			// B.4.3 MIDI IN Jack Descriptor (Embedded)
+			0x06,							// bLength
+			0x24,							// descriptor type
+			0x02,							// MIDI_IN_JACK desc subtype
+			0x01,							// EMBEDDED bJackType
+			0x01,							// bJackID
+			0x00,							// iJack
+
+			// Table B4.4 Midi Out Jack Descriptor (Embedded)
+			0x09,							// length of descriptor in bytes
+			0x24,							// descriptor type
+			0x03,							// MIDI_OUT_JACK descriptor
+			0x01,							// EMBEDDED bJackType
+			0x02,							// bJackID
+			0x01,							// No of input pins
+			0x01,							// ID of the Entity to which this Pin is connected.
+			0x01,							// Output Pin number of the Entity to which this Input Pin is connected.
+			0X00,							// iJack
+
+			//B.5.1 Standard Bulk OUT Endpoint Descriptor
+			0x09,							// bLength
+			0x05,							// bDescriptorType = endpoint
+			0x01,							// bEndpointAddress OUT endpoint number 1
+			0x02,							// bmAttributes: 2:Bulk, 3:Interrupt endpoint
+			0x40, 0X00,						// wMaxPacketSize 64 bytes per packet.
+			0x00,							// bInterval in ms : ignored for bulk
+			0x00,							// bRefresh Unused
+			0x00,							// bSyncAddress Unused
+
+			// B.5.2 Class-specific MS Bulk OUT Endpoint Descriptor
+			0x05,							// bLength of descriptor in bytes
+			0x25,							// bDescriptorType (Audio Endpoint Descriptor)
+			0x01,							// bDescriptorSubtype: MS General
+			0x01,							// bNumEmbMIDIJack
+			0x01,							// baAssocJackID (0) ID of the Embedded MIDI IN Jack.
+
+			//B.6.1 Standard Bulk IN Endpoint Descriptor
+			0x09,							// bLength
+			0x05,							// bDescriptorType = endpoint
+			0x81,							// bEndpointAddress IN endpoint number 1
+			0X02,							// bmAttributes: 2: Bulk, 3: Interrupt endpoint
+			0x40, 0X00,						// wMaxPacketSize
+			0X00,							// bInterval in ms
+			0X00,							// bRefresh
+			0X00,							// bSyncAddress
+
+			// B.6.2 Class-specific MS Bulk IN Endpoint Descriptor
+			0X05,							// bLength of descriptor in bytes
+			0X25,							// bDescriptorType
+			0x01,							// bDescriptorSubtype
+			0X01,							// bNumEmbMIDIJack (0)
+			0X02,							// baAssocJackID (0) ID of the Embedded MIDI OUT Jack
+
 	};
 
+	// Binary Object Store (BOS) Descriptor
 	uint8_t USBD_FS_BOSDesc[0xC] = {
-			0x5,
-			USB_DESC_TYPE_BOS,
-			0xC,
-			0x0,
-			0x1,  /* 1 device capability*/
-			/* device capability*/
-			0x7,
-			0x10, // USB_DEVICE_CAPABITY_TYPE,
-			0x2,
-			0x2,  /* LPM capability bit set*/
-			0x0,
-			0x0,
-			0x0
+			0x05,						// Length
+			USB_DESC_TYPE_BOS,			// DescriptorType
+			0x0C,						// TotalLength
+			0x00, 0x01,					// NumDeviceCaps
+
+			// USB 2.0 Extension Descriptor: device capability
+			0x07,						// bLength
+			0x10, 						// USB_DEVICE_CAPABITY_TYPE
+			0x02,						// Attributes
+			0x02, 0x00, 0x00, 0x00		// Link Power Management protocol is supported
+
 	};
+
 
 	uint8_t USBD_StringSerial[0x1A] = {
 			0x1A,		// size
-			3, 		// USB_DESC_TYPE_STRING
+			0x03, 		// USB_DESC_TYPE_STRING
 	};
 
 	// USB lang indentifier descriptor
